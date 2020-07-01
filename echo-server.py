cdd436f13b99ac8a3b58d87bda957c1e34460678
tanvir43 <raihncse43@gmail.com>
2020-07-01 08:18:28 +0600

    modified socket server

--- echo-server.py
+++ echo-server.py
@@ -1,7 +1,7 @@
 import socket
 
 HOST = "127.0.0.1" # localhost
-PORT = "98765" # port to listen on, non privileged ports are > 1023
+PORT = 65432 # port to listen on, non privileged ports are > 1023
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
